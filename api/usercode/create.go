6fc45f36463f9297d3d295db9c840fa35af0bbb4
daqiangli <dqli161@163.com>
2023-05-06 16:53:21 +0800

    refactor usercode

--- api/usercode/create.go
+++ api/usercode/create.go
@@ -3,57 +3,39 @@ package usercode
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	npool "github.com/NpoolPlatform/message/npool/basal/mw/v1/usercode"
-	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 
 	usercode1 "github.com/NpoolPlatform/basal-middleware/pkg/usercode"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	"github.com/google/uuid"
 )
 
 func (s *Server) CreateUserCode(ctx context.Context, in *npool.CreateUserCodeRequest) (*npool.CreateUserCodeResponse, error) { //nolint
-	if _, err := uuid.Parse(in.GetAppID()); err != nil {
+	handler, err := usercode1.NewHandler(ctx,
+		usercode1.WithAppID(&in.Account),
+		usercode1.WithPrefix(&in.Prefix),
+		usercode1.WithAccount(&in.Account),
+		usercode1.WithAccountType(&in.AccountType),
+		usercode1.WithUsedFor(&in.UsedFor),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateAPI",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.CreateUserCodeResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	if in.GetPrefix() == "" {
-		return &npool.CreateUserCodeResponse{}, status.Error(codes.InvalidArgument, "Prefix is invalid")
-	}
-	if in.GetAccount() == "" {
-		return &npool.CreateUserCodeResponse{}, status.Error(codes.InvalidArgument, "Account is invalid")
-	}
-	switch in.GetAccountType() {
-	case basetypes.SignMethod_Email:
-	case basetypes.SignMethod_Mobile:
-	default:
-		return &npool.CreateUserCodeResponse{}, status.Error(codes.InvalidArgument, "AccountType is invalid")
-	}
-	switch in.GetUsedFor() {
-	case basetypes.UsedFor_Signup:
-	case basetypes.UsedFor_Signin:
-	case basetypes.UsedFor_Update:
-	case basetypes.UsedFor_Contact:
-	case basetypes.UsedFor_SetWithdrawAddress:
-	case basetypes.UsedFor_Withdraw:
-	case basetypes.UsedFor_CreateInvitationCode:
-	case basetypes.UsedFor_SetCommission:
-	case basetypes.UsedFor_SetTransferTargetUser:
-	case basetypes.UsedFor_Transfer:
-	default:
-		return &npool.CreateUserCodeResponse{}, status.Error(codes.InvalidArgument, "UsedFor is invalid")
-	}
-
-	info, err := usercode1.CreateUserCode(
-		ctx,
-		in.GetPrefix(),
-		in.GetAppID(),
-		in.GetAccount(),
-		in.GetAccountType(),
-		in.GetUsedFor())
+	info, err := handler.CreateUserCode(ctx)
 	if err != nil {
+		logger.Sugar().Errorw(
+			"CreateAPI",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.CreateUserCodeResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
