4382f1f0bc75bb7438744a3d9bcf31a3364efc38
daqiangli <dqli161@163.com>
2023-05-06 15:18:58 +0800

    refactor update

--- api/api/update.go
+++ api/api/update.go
@@ -6,25 +6,42 @@ import (
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
+	api1 "github.com/NpoolPlatform/basal-middleware/pkg/api"
+	npool "github.com/NpoolPlatform/message/npool/basal/mw/v1/api"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	mgrcli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
-	npool "github.com/NpoolPlatform/message/npool/basal/mw/v1/api"
-
-	"github.com/google/uuid"
 )
 
 func (s *Server) UpdateAPI(ctx context.Context, in *npool.UpdateAPIRequest) (*npool.UpdateAPIResponse, error) {
-	var err error
-
-	if _, err := uuid.Parse(in.GetInfo().GetID()); err != nil {
+	req := in.GetInfo()
+	handler, err := api1.NewHandler(ctx,
+		api1.WithID(req.ID),
+		api1.WithProtocol(req.Protocol),
+		api1.WithServiceName(req.ServiceName),
+		api1.WithMethod(req.Method),
+		api1.WithMethodName(req.MethodName),
+		api1.WithPath(req.Path),
+		api1.WithPathPrefix(req.PathPrefix),
+		api1.WithDomains(&req.Domains),
+		api1.WithDeprecated(req.Depracated),
+		api1.WithExported(req.Exported),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateAPI",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.UpdateAPIResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	info, err := mgrcli.UpdateAPI(ctx, in.GetInfo())
+	info, err := handler.UpdateAPI(ctx)
 	if err != nil {
-		logger.Sugar().Errorf("fail update api: %v", err.Error())
+		logger.Sugar().Errorw(
+			"UpdateAPI",
+			"In", in,
+			"Error", err,
+		)
 		return &npool.UpdateAPIResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
---
+++ pkg/api/update.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"context"
+
+	crud "github.com/NpoolPlatform/basal-middleware/pkg/crud/api"
+	"github.com/NpoolPlatform/basal-middleware/pkg/db"
+	"github.com/NpoolPlatform/basal-middleware/pkg/db/ent"
+	npool "github.com/NpoolPlatform/message/npool/basal/mw/v1/api"
+)
+
+func (h *Handler) UpdateAPI(ctx context.Context) (info *npool.API, err error) {
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := crud.UpdateSet(
+			cli.API.UpdateOneID(*h.ID),
+			&crud.Req{
+				Protocol:    h.Protocol,
+				Method:      h.Method,
+				MethodName:  h.MethodName,
+				Path:        h.Path,
+				PathPrefix:  h.PathPrefix,
+				ServiceName: h.ServiceName,
+				Exported:    h.Exported,
+				Depracated:  h.Deprecated,
+				Domains:     h.Domains,
+			},
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+}
