4867dc742e5712c1870ed96d0efa6d87bd605a76
daqiangli <dqli161@163.com>
2023-05-08 15:25:34 +0800

    bugfix:AppID

--- api/usercode/create.go
+++ api/usercode/create.go
@@ -14,7 +14,7 @@ import (
 
 func (s *Server) CreateUserCode(ctx context.Context, in *npool.CreateUserCodeRequest) (*npool.CreateUserCodeResponse, error) { //nolint
 	handler, err := usercode1.NewHandler(ctx,
-		usercode1.WithAppID(&in.Account),
+		usercode1.WithAppID(&in.AppID),
 		usercode1.WithPrefix(&in.Prefix),
 		usercode1.WithAccount(&in.Account),
 		usercode1.WithAccountType(&in.AccountType),
